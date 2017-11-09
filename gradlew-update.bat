2bba6a18e1ff6cc1cb7575362f3b0c765cbf695f
Adrian Minde <Adrian_Minde@live.de>
2017-11-09 17:33:36 +0100

    OpenMUC 0.17.7 | 2017.11.09

--- gradlew-update.bat
+++ gradlew-update.bat
@@ -1,5 +1,5 @@
 ::BATCH file for windows
 
-call gradlew.bat build
 call gradlew.bat -b framework/conf/bundles.conf.gradle updateBundles
+call gradlew.bat -b ../emonmuc/conf/bundles.conf.gradle updateBundles
 pause
