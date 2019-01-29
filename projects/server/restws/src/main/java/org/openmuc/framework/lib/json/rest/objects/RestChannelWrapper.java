1dbd7f3516afa461476457832ba17f297372d2ef
Adrian Minde <Adrian_Minde@live.de>
2019-01-29 16:33:43 +0100

    Fix rest server error for undefined valueTypes

--- projects/server/restws/src/main/java/org/openmuc/framework/lib/json/rest/objects/RestChannelWrapper.java
+++ projects/server/restws/src/main/java/org/openmuc/framework/lib/json/rest/objects/RestChannelWrapper.java
@@ -46,7 +46,10 @@ public class RestChannelWrapper {
     }
 
     public ValueType getValueType() {
-    	return config.getValueType();
+    	if (config.getValueType() != null) {
+    		return config.getValueType();
+    	}
+    	return ChannelConfig.VALUE_TYPE_DEFAULT;
     }
 
     public String getDriver() {
