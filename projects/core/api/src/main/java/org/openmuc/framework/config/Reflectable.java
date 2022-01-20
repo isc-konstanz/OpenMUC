d90033af8c9689ce05b0d1eca0bde7d6707f223e
Adrian Minde <adrian_minde@live.de>
2022-01-20 16:53:09 +0100

    Make Reflectable method check argument sensitive

--- projects/core/api/src/main/java/org/openmuc/framework/config/Reflectable.java
+++ projects/core/api/src/main/java/org/openmuc/framework/config/Reflectable.java
@@ -46,50 +46,39 @@ public abstract class Reflectable extends Configurable {
                    long.class,    Long.class,
                    short.class,   Short.class);
 
-    public static <A extends Annotation> List<Method> getMethods(Class<A> annot, Class<?> type) throws RuntimeException {
+    public static <A extends Annotation> List<Method> getMethods(Class<A> annot, Class<?> type, Object[] args)
+            throws RuntimeException {
+
         List<Method> methods = new ArrayList<Method>();
         while(type.getSuperclass() != null) {
             methods.addAll(Arrays.asList(type.getDeclaredMethods()));
             type = type.getSuperclass();
         }
-        methods.removeIf(m -> !m.isAnnotationPresent(annot));
-        
+        methods.removeIf(m -> !m.isAnnotationPresent(annot) || !hasParameterTypes(m, args));
+
         return methods;
     }
 
-    protected <A extends Annotation> boolean hasMethod(Class<A> annot, Class<?> type) throws RuntimeException {
-        return getMethods(annot, type).size() > 0;
+    protected <A extends Annotation> boolean hasMethod(Class<A> annot, Class<?> type, Object... args)
+            throws RuntimeException {
+
+        return getMethods(annot, type, args).size() > 0;
     }
 
-    protected <A extends Annotation> boolean hasMethod(Class<A> annot, Object obj) throws RuntimeException {
-        return hasMethod(annot, obj.getClass());
+    protected <A extends Annotation> boolean hasMethod(Class<A> annot, Object obj, Object... args)
+            throws RuntimeException {
+
+        return hasMethod(annot, obj.getClass(), args);
     }
 
     protected <A extends Annotation> void invokeMethod(Class<A> annot, Object obj, Object... args) {
-        List<Method> methods = getMethods(annot, obj.getClass());
+        List<Method> methods = getMethods(annot, obj.getClass(), args);
         if (methods.size() < 1) {
-            logger.trace("Skipping invocation of nonexisting method with annotation: {}", annot.getSimpleName());
+            logger.trace("Skipping invocation of nonexisting {} method for arguments: {}", annot.getSimpleName(),
+                    List.of(args).stream().map(a -> a.getClass()).toArray(Class<?>[]::new));
             return;
         }
-        methods:
         for (Method method : methods) {
-            if (method.getParameterCount() != args.length) {
-                logger.trace("Skipping invocation of method \"{}\" with nonmatching amount of arguments: {}", method.getName(), 
-                        method.getParameterCount());
-                
-                continue methods;
-            }
-            else if (method.getParameterCount() > 0) {
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                for (int i=0; i<args.length; i++) {
-                    if (!isAssignableTo(args[i].getClass(), parameterTypes[i])) {
-                        logger.trace("Skipping invocation of method \"{}\" with nonmatching arguments: {} != {}", method.getName(), 
-                                args[i].getClass().getSimpleName(), parameterTypes[i].getSimpleName());
-                        
-                        continue methods;
-                    }
-                }
-            }
             try {
                 method.invoke(obj, args);
 
@@ -103,23 +92,21 @@ public abstract class Reflectable extends Configurable {
     }
 
     protected <A extends Annotation> Object invokeReturn(Class<A> annot, Object obj, Object... args) {
-        Class<?>[] argTypes = List.of(args).stream().map(a -> a.getClass()).toArray(Class<?>[]::new);
-        List<Method> methods = getMethods(annot, obj.getClass());
+        List<Method> methods = getMethods(annot, obj.getClass(), args);
         for (Method method : methods) {
-        	if (Arrays.equals(method.getParameterTypes(), argTypes)) {
-                try {
-                    return method.invoke(obj, args);
-                    
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                    
-                } catch (InvocationTargetException e) {
-                    throwException(e.getCause());
-                }
-        	}
+            try {
+                return method.invoke(obj, args);
+                
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+                
+            } catch (InvocationTargetException e) {
+                throwException(e.getCause());
+            }
         }
         throw new UnsupportedOperationException(MessageFormat.format("Unable to find {0} method \"{1}\" for arguments: {2}", 
-        		annot.getSimpleName(), obj.getClass().getSimpleName(), argTypes));
+                annot.getSimpleName(), obj.getClass().getSimpleName(),
+                List.of(args).stream().map(a -> a.getClass()).toArray(Class<?>[]::new)));
     }
 
     public static <C extends Configurable> C newInstance(Class<C> configurable) throws RuntimeException {
@@ -136,6 +123,27 @@ public abstract class Reflectable extends Configurable {
     @SuppressWarnings("unchecked")
     private static <T extends Throwable> void throwException(Throwable exception) throws T {
         throw (T) exception;
+    }
+
+    public static boolean hasParameterTypes(Method method, Object... args) {
+        if (method.getParameterCount() != args.length) {
+            logger.trace("Removing method \"{}\" with nonmatching amount of arguments: {}", method.getName(), 
+                    method.getParameterCount());
+            
+            return false;
+        }
+        else if (method.getParameterCount() > 0) {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (int i=0; i<args.length; i++) {
+                if (!isAssignableTo(args[i].getClass(), parameterTypes[i])) {
+                    logger.trace("Removing method \"{}\" with nonmatching arguments: {} != {}", method.getName(), 
+                            args[i].getClass().getSimpleName(), parameterTypes[i].getSimpleName());
+                    
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static boolean isAssignableTo(Class<?> targetClass, Class<?> parameterClass) {
