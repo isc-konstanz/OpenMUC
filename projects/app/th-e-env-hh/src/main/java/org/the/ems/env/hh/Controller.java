d42b687ed46fc877ec5475f5b7636c55f8013794
derpdounat <daniel.bohnet.millan@gmail.com>
2023-06-28 11:21:32 +0200

    Add consideration of past not reached errors

--- projects/app/th-e-env-hh/src/main/java/org/the/ems/env/hh/Controller.java
+++ projects/app/th-e-env-hh/src/main/java/org/the/ems/env/hh/Controller.java
@@ -20,36 +20,75 @@
  */
 package org.the.ems.env.hh;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import org.openmuc.framework.data.DoubleValue;
+import org.openmuc.framework.data.Flag;
+import org.openmuc.framework.data.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Controller {
 
     protected double errorProportional = 0;
     protected double errorIntegral = 0;
     protected double errorDerivative = 0;
     protected double errorLast = 0;
+    protected double lastErrors = 0;
+    protected double lastMeasurements = 0;
 
     protected double proportional;
     protected double integral;
     protected double derivative;
     protected double maximum;
     protected double minimum;
+    protected boolean delta;
+ 
+    private final Deque<Record> errors = new ArrayDeque<Record>();
+    private final Deque<Record> measurement = new ArrayDeque<Record>();
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    private double lastsetpoint = 0;
-    private double deltavalue = 0;
-
-    public Controller(double proportional,double integral, double derivative, double maximum,double minimum) {
+    public Controller(double proportional,double integral, double derivative, double maximum,double minimum, boolean delta) {
         this.proportional = proportional;
         this.derivative = derivative;
         this.integral = integral;
         this.maximum = maximum;
         this.minimum = minimum;
-
+        this.delta = delta;
     }
 
     public double process(long interval, double thPowerSetpoint, double thPowerSensor) {
-        double timeDelta = interval/1000;
-        deltavalue = lastsetpoint - thPowerSensor;
-        lastsetpoint = thPowerSetpoint;
-        double error = thPowerSetpoint - thPowerSensor + deltavalue;
+        double timeDelta = interval;
+        double error = thPowerSetpoint - thPowerSensor;
+        if(delta) {
+            if (!errors.isEmpty()) {
+                if (errors.getLast().getFlag() == Flag.VALID) {
+                    lastErrors = errors.stream().mapToDouble(r -> r.getValue().asDouble()).average().orElse(0);
+                    lastMeasurements = measurement.stream().mapToDouble(r -> r.getValue().asDouble()).average().orElse(0);
+                    lastErrors = lastErrors - lastMeasurements;
+                    if (Math.abs(lastErrors) < 200) {
+                        logger.debug("Delta of Lasterrors smaller than 200 W, removing past errors!");
+                        lastErrors = 0;
+                        lastMeasurements = 0;
+                        errors.clear();
+                        measurement.clear();
+                    }
+                }
+            }
+
+            errors.add(new Record(new DoubleValue(error),System.currentTimeMillis()));
+            measurement.add(new Record(new DoubleValue(thPowerSensor),System.currentTimeMillis()));
+            error = error + lastErrors;
+            if (errors.size() > 20) {
+                logger.trace("Bigger thatn 20 items in errors deque, starting clearing");
+                errors.removeFirst();
+                measurement.removeFirst();
+                while (errors.getFirst().getValue().asDouble() < 0) {
+                    errors.removeFirst();
+                    measurement.removeFirst();
+                }
+            }
+        }
 
         errorProportional = proportional * error;
         errorIntegral = lim(errorIntegral + integral * error * timeDelta);
@@ -72,8 +111,6 @@ public class Controller {
         if (value > maximum ) {
             return maximum;
         }
-        if (value < -maximum) {
-            return -maximum;
         if (value <= minimum) {
             return minimum;
         }
